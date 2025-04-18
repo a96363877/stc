ac36153c3673c7c2d26a0b085cbb755f50086f8f
a96363877 <a96363877@gmail.com>
2025-04-18 14:35:07 +0300

    sad

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
+import { GoogleTagManager } from "@next/third-parties/google"
 
 // Load local Arabic font
 
@@ -18,10 +19,12 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <head>
+        
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
       </head>
       <body className={`min-h-screen bg-background antialiased font-sans`}>
           {children}
+          <GoogleTagManager gtmId="AW-410329319" />
       </body>
     </html>
   )
